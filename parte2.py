639e6204375407564247a7b0a5f5767f74bdc0e0
Mr-Gabito <a01709333@tec.mx>
2024-08-20 12:26:14 -0600

    Update parte2.py

---
+++ parte2.py
@@ -0,0 +1,6 @@
+r=int(input("Dame un numero para el radio del cono: "))
+h=int(input("Dame un numero para la altura del cono: "))
+pi=3.141592
+
+volumen=1/3*pi*r**2*h
+print("El volumen de tu cono es: ",volumen)
