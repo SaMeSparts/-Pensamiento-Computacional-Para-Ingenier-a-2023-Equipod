5a788d48d28e05ac56ed78510f929da1e072ca2d
SaMeSparts <132626666+SaMeSparts@users.noreply.github.com>
2024-08-20 12:31:23 -0600

    Update parte2.py

--- parte2.py
+++ parte2.py
@@ -3,4 +3,4 @@ h=int(input("Dame un numero para la altura del cono: "))
 pi=3.141592
 
 volumen=1/3*pi*r**2*h
-print("El volumen de tu cono es: ",volumen)
+print("El volumen de tu cono es: ","%.3f"%volumen)
